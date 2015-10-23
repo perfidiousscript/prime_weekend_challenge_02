cafcfba6bf6e1225cd136b39bd5daa11d6156d76
Sam <sam.moss321@gmail.com>
2015-10-23 18:12:34 -0500

    numbers on buttons

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,72 +1,63 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     $.ajax({
-        url:"/data",
-        success: function(data){
-            console.log(data);
-        }
-    })
+        url: "/data",
+        success: function (data) {
+            var peopleArray = data.zeta;
+            var indexTracker = 0;
 
-    var peopleArray = ["Dana", "Pui", "Kelly", "Sam"];
+            createCarousel(peopleArray);
+            updateIndexPoints();
+            $("#next").on('click', nextSlide);
+            $("#prev").on('click', prevSlide);
+            function createCarousel(array) {
+                $("#lecture").append("<div class='main'></div>");
+                var $el = $("#lecture").children().last();
+                createIndexPoints(array, $el);
+                createNavButtons($el);
+            }
 
-    var indexTracker = 0;
+            function nextSlide() {
+                indexTracker++;
+                if (indexTracker >= peopleArray.length) {
+                    indexTracker = 0;
+                }
 
-    $(document).ready(function(){
-        createCarousel(peopleArray);
+                updateIndexPoints();
+            }
 
-        updateIndexPoints();
+            function prevSlide() {
+                indexTracker--;
+                if (indexTracker < 0) {
+                    indexTracker = peopleArray.length - 1;
+                }
 
-        $("#next").on('click', nextSlide);
-        $("#prev").on('click', prevSlide);
-    });
+                updateIndexPoints();
+            }
 
-    function createCarousel(array){
-        $("#lecture").append("<div class='main'></div>");
-        var $el = $("#lecture").children().last();
-        createIndexPoints(array, $el);
-        createNavButtons($el);
-    }
+            function createNavButtons($el) {
+                $("#lecture").children().prepend("<div id='prev' class='nav-button'><span><</span></div>");
+                $el.append("<div id='next' class='nav-button'><span>></span></div>");
+            }
 
-    function nextSlide(){
-        indexTracker++;
-        if(indexTracker >= peopleArray.length){
-            indexTracker = 0;
-        }
+            function createIndexPoints(array, $el) {
+                //create something visual, Divs will work
+                for (var i = 0; i < array.length; i++) {
+                    //we need i, 1 for each element
+                    $el.append("<div class='index-point' id='index" + i + "'>"+ i +"</div>")
 
-        updateIndexPoints();
-    }
+                }
+            }
 
-    function prevSlide(){
-        indexTracker--;
-        if(indexTracker < 0){
-            indexTracker = peopleArray.length - 1;
-        }
+            function updateIndexPoints() {
+                for (var i = 0; i < peopleArray.length; i++) {
+                    $("#index" + i).removeClass("index-point-active");
 
-        updateIndexPoints();
-    }
-
-    function createNavButtons($el){
-        $("#lecture").children().prepend("<div id='prev' class='nav-button'><span><</span></div>");
-        $el.append("<div id='next' class='nav-button'><span>></span></div>");
-    }
-
-    function createIndexPoints(array, $el){
-        //create something visual, Divs will work
-        for(var i = 0; i < array.length; i++){
-            //we need i, 1 for each element
-            $el.append("<div class='index-point' id='index" + i + "'></div>")
-
-        }
-    }
-
-    function updateIndexPoints(){
-        for(var i = 0; i < peopleArray.length; i++){
-            $("#index" + i).removeClass("index-point-active");
-
-            if(i == indexTracker){
-                $("#index" + i).addClass("index-point-active");
+                    if (i == indexTracker) {
+                        $("#index" + i).addClass("index-point-active");
+                    }
+                }
             }
         }
-    }
-
+    })
 });
