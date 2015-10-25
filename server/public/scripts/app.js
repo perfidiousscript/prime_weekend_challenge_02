0a896cc69dfee86d73ef03462616d2e8db171db9
Sam <sam.moss321@gmail.com>
2015-10-25 12:40:33 -0500

    function advance timer

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,6 +1,14 @@
+//This is a script this creates a text carousel. The script pulls down a JSON object containing and array
+// of objects. Each object had three elements, a name, a github URL and s shout out text.
+// The script populates three divs with the elements from one object.
+
+//'peopleArray' will hold the data pulled from the server, 'indexTrack' holds the number that specifies
+//which object's information will be presented.
 var peopleArray = [], indexTracker = 0;
 
+//Main jQuery script.
 $(document).ready(function() {
+    //ajax call pulls down the object from server.
     $.ajax({
         type : "GET",
         url  : "/data",
@@ -12,25 +20,27 @@ $(document).ready(function() {
             createCarousel(peopleArray);
 
             updateIndexPoints();
-
+            //Advances the carousel when the next button is clicked.
             $("#next").on('click', nextSlide);
 
             $("#prev").on('click', prevSlide);
-
-            window.setInterval(nextSlide, 10000);
+            //Advances the carousel after ten seconds of user inactivity.
         }
     });
 });
 
 
-
+//Creates a div called main and calls functions that populate the index points and nav buttons.
 function createCarousel(array) {
     $("#lecture").append("<div class='main'></div>");
     var $el = $("#lecture").children().last();
     createIndexPoints(array, $el);
     createNavButtons($el);
+    var nextInterval = window.setInterval(nextSlide, 10000);
 }
 
+//Function which updates the indexTracker and active point when advanced. Also updates information presented in the
+// currentInterval div with the object specified by indexTracker.
 function nextSlide() {
     indexTracker++;
     if (indexTracker >= peopleArray.length) {
@@ -39,9 +49,11 @@ function nextSlide() {
     allFadeOut();
     window.setTimeout(updateIndexPoints,500);
     allFadeIn();
-
+    intervalReset();
 }
 
+//Function which updates the indexTracker and active point when reversed. Also updates information presented in the
+// currentInterval div with the object specified by indexTracker.
 function prevSlide() {
     indexTracker--;
     if (indexTracker < 0) {
@@ -50,6 +62,7 @@ function prevSlide() {
     allFadeOut();
     window.setTimeout(updateIndexPoints,500);
     allFadeIn();
+    intervalReset();
 
 }
 //Prepends a div to the lecture div that reverses the carousel when clicked and appends a div to the lecture
@@ -59,7 +72,8 @@ function createNavButtons($el) {
     $el.append("<div id='next' class='nav-button'><span>></span></div>");
 }
 
-
+//Function which populates a number of index points equivalent to the number of elements in the
+//uploaded object.
 function createIndexPoints(array, $el) {
     for (var i = 0; i < array.length; i++) {
         $el.append("<div class='index-point' id='index" + i + "'><div class='num'>"+ i +"</div></div>")
@@ -67,6 +81,7 @@ function createIndexPoints(array, $el) {
     }
 }
 
+//Changes the active index point to that index point specified bu the index tracker variable.
 function updateIndexPoints() {
     for (var i = 0; i < peopleArray.length; i++) {
         $("#index" + i).removeClass("index-point-active");
@@ -87,14 +102,21 @@ function updateCurrentStudent(position){
     $('#studentShoutout').text(peopleArray[position].shoutout);
 }
 
+//Fades out all divs with class 'info'
 function allFadeOut(){
-    $('#studentName').fadeOut();
-    $('#studentGithub').fadeOut();
-    $('#studentShoutout').fadeOut();
+    $('.info').fadeOut();
+
 }
 
+//Fades in all divs with class 'info'
 function allFadeIn(){
-    $('#studentName').fadeIn();
-    $('#studentGithub').fadeIn();
-    $('#studentShoutout').fadeIn();
+    $('.info').fadeIn();
+}
+
+
+function intervalReset(){
+    clearInterval(nextInterval);
+    setTimeout(function() {
+        window.setInterval(nextSlide, 10000);
+    }, 10000);
 }
